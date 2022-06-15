e53067c9b936a55a86ed603d7f3b9f11d755810a
ngo minh ngoc <ngoc.ngominh1711@gmail.com>
2022-06-15 09:04:47 +0700

    add file

--- test.js
+++ test.js
@@ -1,11 +1,23 @@
-console.log()
-{
-  var i = 5;
+let F = 0 , F0= 0, F1=0
+  let chia5 = ""
+  let fibonacci = "";
+  for (let i=0; i<20;i++){
+    if(i==0){
+      F=1
+      F1=1
+      fibonacci += F+ " "
+    }else{
 
-  var count = 0;
-
-  for (var i = 0; i <= 10; i += 2) {
-    count += i;
-    console.log(count)
+      F= F0+F1
+      F0 = F1
+      F1 = F
+      fibonacci += F+ " "
+      console.log(i +" = "+F)
+      if(F%5==0){
+        chia5 = F
+        break
+      }
+    }
   }
-}
+
+  document.write("Sô chia hết cho 5 trong dãy là "+chia5)
