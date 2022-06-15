57c9bc6b4a639530a795ba4b1239e19deede4f14
ngo minh ngoc <ngoc.ngominh1711@gmail.com>
2022-06-15 21:53:09 +0700

    update file

--- hinhchunhat.js
+++ hinhchunhat.js
@@ -1,10 +1,2 @@
 
-let inputwidth =prompt("nhap chieu rong");
-document.write("chieu rong = " + inputwidth + '<br>')
-let inputheight =prompt("nhap chieu cao");
-document.write(" chieu cao = " +inputheight + '<br>')
-let width= parseInt(inputwidth);
-let height= parseInt(inputheight);
-let area = width * height;
-document.write("the area is " + area)
 
